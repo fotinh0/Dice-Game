f7cd5ed8bfc2e51bdceafa9e02664c8131fce854
Foti Ceci <foti.ceci@my.liu.edu>
2021-08-24 01:17:46 -0400

    Upload script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// JS file
+// Generates two random numbers
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Changes src path of image to match generated random number with dice image
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 +".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 +".png");
+
+//  Checks for winner
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!";
+} 
+else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins! 🏆";
+}
+else {
+    document.querySelector("h1").textContent = "Player 2 Wins! 🏆";
+}
